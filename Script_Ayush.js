78a0f511c2b20a5fa0bf940c005b341ed8946013
som-random-dude <s.randomperson632@gmail.com>
2025-12-02 11:54:23 +0545

    removed some unnecessary code

--- Script_Ayush.js
+++ Script_Ayush.js
@@ -1,6 +1,5 @@
 let search = document.getElementById('searchInput');
-let mainCointainer = document.getElementsByClassName('product-grid')[0];
-const h3 = document.createElement('h3');
+
 search.addEventListener('input', function(event) {
     let cointainer = document.getElementsByClassName('product-card');
     const searchTerm = event.target.value.toLowerCase();
@@ -23,9 +22,6 @@ search.addEventListener('input', function(event) {
         else{
              cointainer[i].style.display = "none";
         }
-        if (document.getElementsByClassName('product-grid').length === 0){
-            h3.innerHTML = "results not found";
-            mainCointainer.appendChild(h3);
-        }
+        
     }
 });
